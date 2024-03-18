d3d33f478b3d0205335cf0fab4eb02a381b18929
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-03-19 00:22:00 +0100

    adds content to mar1 and a small fix to feb9

--- util/readPages.js
+++ util/readPages.js
@@ -10,41 +10,41 @@ export const homepagePage = renderPage(homepage, {
 const feb2nd = readPage(`./public/pages/feb2nd/feb2nd.html`)
 export const feb2ndPage = renderPage(feb2nd, {
     tabTitle: `Feb 2nd overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/feb2nd.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const feb9 = readPage(`./public/pages/feb9/feb9.html`)
 export const feb9Page = renderPage(feb9, {
     tabTitle: `Feb 9th overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/feb9.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const feb16 = readPage(`./public/pages/feb16/feb16.html`)
 export const feb16Page = renderPage(feb16, {
     tabTitle: `Feb 16th overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/feb16.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const feb23 = readPage(`./public/pages/feb23/feb23.html`)
 export const feb23Page = renderPage(feb23, {
     tabTitle: `Feb 23rd overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/feb23.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const mar1 = readPage(`./public/pages/mar1/mar1.html`)
 export const mar1Page = renderPage(mar1, {
     tabTitle: `March 1st overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/mar1.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const mar8 = readPage(`./public/pages/mar8/mar8.html`)
 export const mar8Page = renderPage(mar8, {
     tabTitle: `March 8th overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/mar8.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
 
 const mar15 = readPage(`./public/pages/mar15/mar15.html`)
 export const mar15Page = renderPage(mar15, {
     tabTitle: `March 15th overview`,
-    CSSLinks: `<link rel="stylesheet" href="../../assets/css/mar15.css">`
+    CSSLinks: `<link rel="stylesheet" href="../../assets/css/contentPages.css">`
 });
